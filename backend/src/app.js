d7c88544250fc4cb1942556d6bb55a5e0abeca8a
Lakhwinder-bit <singhlakhiwinder3@gmail.com>
2026-05-16 17:47:15 +0530

    backend cores change

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,11 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://authcation-system.vercel.app/",
+  origin: [
+    "http://localhost:5173",
+    "https://authcation-system.vercel.app"
+
+  ],
   credentials: true
 }));
 
